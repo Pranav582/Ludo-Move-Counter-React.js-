a1ae9283ce8dc0ab206bbc34eb583e4e637ebefb
Pranav582 <pranavsagne37@gmail.com>
2025-05-09 00:45:28 +0530

    Added LudoBoard game files

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,17 +1,17 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// export default function Counter() {
-//   const [count, setCount] = useState(0);
+export default function Counter() {
+  const [count, setCount] = useState(0);
 
-//   function incCount() {
-//     setCount(count + 2);
-//   }
+  function incCount() {
+    setCount(count + 2);
+  }
 
-//   return (
-//     <div>
-//       <h3>Count = {count}</h3>
-//       <button onClick={incCount}>Increase</button>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <h3>Count = {count}</h3>
+      <button onClick={incCount}>Increase</button>
+    </div>
+  );
+}
  
